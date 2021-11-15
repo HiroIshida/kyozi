d972c4f92be6a71704b1b996a2701d11388fd2c3
HiroIshida <spitfire.docomo@gmail.com>
2021-11-15 11:25:17 +0900

    Update debug

--- scripts/debug.py
+++ scripts/debug.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import rospkg
+import argparse
 import sys
 import os
 import time
@@ -16,19 +18,17 @@ try:
 except:
     _has_skrobot = False
 
-from utils import get_cache_directory
+from utils import get_project_directory, get_cache_directory
 from utils import load_pickle_6compat
+from utils import construct_config
 
-def get_sample_data():
-    cache_dir = get_cache_directory()
-    files = [os.path.join(cache_dir, fname) for fname in os.listdir(cache_dir)]
-    cache_files = [fn for fn in files if os.path.splitext(fn)[1]=='.cache']
+def get_sample_data(config):
+    project_dir = get_project_directory(config)
+    files = [os.path.join(project_dir, fname) for fname in os.listdir(project_dir)]
     chunk_files = [fn for fn in files if os.path.splitext(fn)[1]=='.pickle']
     assert len(chunk_files) == 1
 
-    sequence = load_pickle_6compat(cache_files[0])
     chunk = load_pickle_6compat(chunk_files[0])
-    joint_names = sequence.joint_names
     cmd_seq = chunk['cmd_seqs'][0]
     img_seq = chunk['img_seqs'][0]
 
@@ -37,13 +37,21 @@ def get_sample_data():
 
     assert isinstance(img_seq, np.ndarray)
     assert img_seq.ndim == 4 # (n_seq, pix, pix, channel)
-    return cmd_seq, img_seq, joint_names
+    return cmd_seq, img_seq
 
 if __name__=='__main__':
-    cmd_seq, img_seq, joint_names = get_sample_data()
+    config_path = os.path.join(rospkg.RosPack().get_path('kyozi'), 'config')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-config', type=str, default=os.path.join(config_path, 'pr2_rarm.yaml'))
+    args = parser.parse_args()
+    config_file = args.config
+    config = construct_config(config_file)
+
+    cmd_seq, img_seq = get_sample_data(config)
 
     if _has_moviepy:
-        cache_dir = get_cache_directory()
+        cache_dir = get_project_directory(config)
         filename_gif = os.path.join(cache_dir, "sample.gif")
         clip = ImageSequenceClip([img for img in img_seq], fps=50)
         clip.write_gif(filename_gif, fps=50)
@@ -55,7 +63,7 @@ if __name__=='__main__':
         viewer.show()
         time.sleep(2)
         for cmd in cmd_seq:
-            for jn, angle in zip(joint_names, cmd):
+            for jn, angle in zip(config.joint_names, cmd):
                 joint = robot.__dict__[jn]
                 joint.joint_angle(angle)
             viewer.redraw()
