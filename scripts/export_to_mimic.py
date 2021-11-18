abdab9d92ee77f0f78bbd1baba32669f2d9cdaf1
HiroIshida <spitfire.docomo@gmail.com>
2021-11-18 15:24:03 +0900

    Load pickle using 6 compat

--- scripts/export_to_mimic.py
+++ scripts/export_to_mimic.py
@@ -3,6 +3,7 @@ import argparse
 import rospkg
 import pickle
 from utils import construct_config
+from utils import load_pickle_6compat
 from mimic.datatype import ImageCommandDataChunk
 
 # HiroIshida's personal project
@@ -12,15 +13,14 @@ if __name__=='__main__':
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-config', type=str, default=os.path.join(config_path, 'pr2_rarm.yaml'))
-    parser.add_argument('-pn', type=str, default='project name')
+    parser.add_argument('-pn', type=str, default='real_robot')
     args = parser.parse_args()
     config_file = args.config
     project_name = args.pn
 
     config = construct_config(config_file)
     filename = os.path.expanduser(os.path.join(config.project_path, 'summary_chunk.pickle'))
-    with open(filename, 'rb') as f:
-        data = pickle.load(f)
+    data = load_pickle_6compat(filename)
 
     chunk = ImageCommandDataChunk()
     for img_seq, cmd_seq in zip(data['img_seqs'], data['cmd_seqs']):
