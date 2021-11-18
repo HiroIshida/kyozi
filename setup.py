613ef58051137f6957d92956bdfd816d4a35e2c1
HiroIshida <spitfire.docomo@gmail.com>
2021-11-19 04:11:17 +0900

    As a python package

---
+++ setup.py
@@ -0,0 +1,15 @@
+import sys
+from setuptools import setup, find_packages
+
+setup_requires = []
+
+install_requires = [
+        ]
+
+setup(
+    name='kyozi',
+    version='0.0.1',
+    description='',
+    license=license,
+    install_requires=install_requires,
+)
