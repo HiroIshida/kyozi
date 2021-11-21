2d7b2d7f3faa26d3772af0560d53f38dbca941fe
HiroIshida <spitfire.docomo@gmail.com>
2021-11-21 15:28:00 +0900

    Add strip argument

--- kyozi/export_to_mimic.py
+++ kyozi/export_to_mimic.py
@@ -14,9 +14,11 @@ if __name__=='__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-config', type=str, default=os.path.join(config_path, 'pr2_rarm.yaml'))
     parser.add_argument('-pn', type=str, default='real_robot')
+    parser.add_argument('--strip-depth', action='store_true', help='strip depth if depth info included')
     args = parser.parse_args()
     config_file = args.config
     project_name = args.pn
+    strip_depth = args.strip_depth
 
     config = construct_config(config_file)
     filename = os.path.expanduser(os.path.join(config.project_path, 'summary_chunk.pickle'))
@@ -25,4 +27,9 @@ if __name__=='__main__':
     chunk = ImageCommandDataChunk()
     for img_seq, cmd_seq in zip(data['img_seqs'], data['cmd_seqs']):
         chunk.push_epoch((img_seq, cmd_seq))
+    if strip_depth:
+        assert chunk.with_depth
+        chunk = chunk.to_depth_stripped()
+        assert not chunk.with_depth
+        print("successfully stripped")
     chunk.dump(project_name)
